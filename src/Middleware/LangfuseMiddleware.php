18d7a9a4fa414ed23b089933c7fcd78fd3a1cae8
Jan Zaba <janekzaba@gmail.com>
2024-11-14 23:51:23 +0100

    Fix guzzle middleware

--- src/Middleware/LangfuseMiddleware.php
+++ src/Middleware/LangfuseMiddleware.php
@@ -6,6 +6,7 @@ use GuzzleHttp\Middleware;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Langfuse\Client\LangfuseClient;
+use GuzzleHttp\Promise\PromiseInterface;
 
 class LangfuseMiddleware
 {
@@ -22,8 +23,17 @@ class LangfuseMiddleware
             function (RequestInterface $request, array $options) {
                 $this->langfuseClient->processRequest($request);
             },
-            function (RequestInterface $request, array $options, ResponseInterface $response) {
-                $this->langfuseClient->processResponse($response, $request);
+            function (RequestInterface $request, array $options, PromiseInterface $promise) {
+                $promise->then(
+                    function (ResponseInterface $response) use ($request) {
+                        $this->langfuseClient->processResponse($response, $request);
+                    },
+                    function ($reason) use ($request) {
+                        // Handle exceptions if needed
+                        // $reason may be an Exception
+                        error_log('Request failed: ' . $reason);
+                    }
+                );
             }
         );
     }
